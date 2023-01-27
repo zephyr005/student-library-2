27e5d60ac4cedd1051ca16c39a9c0e04c3ec7070
Ashish Rai <mandakinirai2410@gmail.com>
2023-01-27 16:10:21 +0530

    5

--- src/main/java/com/driver/services/AuthorService.java
+++ src/main/java/com/driver/services/AuthorService.java
@@ -1,7 +1,7 @@
 package com.driver.services;
 
-import com.driver.models.Author;
 import com.driver.repositories.AuthorRepository;
+import com.driver.models.Author;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +9,9 @@ import org.springframework.stereotype.Service;
 public class AuthorService {
 
     @Autowired
-    AuthorRepository authorRepository;
+    AuthorRepository authorRepository1;
 
     public void create(Author author){
-        authorRepository.save(author);
+        authorRepository1.save(author);
     }
 }
-
--- src/main/java/com/driver/services/CardService.java
+++ src/main/java/com/driver/services/CardService.java
@@ -1,9 +1,9 @@
 package com.driver.services;
 
+import com.driver.repositories.CardRepository;
 import com.driver.models.Card;
 import com.driver.models.CardStatus;
 import com.driver.models.Student;
-import com.driver.repositories.CardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,18 +11,18 @@ import org.springframework.stereotype.Service;
 public class CardService {
 
     @Autowired
-    CardRepository cardRepository;
+    CardRepository cardRepository3;
 
     public Card createAndReturn(Student student){
         Card card = new Card();
         card.setStudent(student);
         student.setCard(card);
 
-        cardRepository.save(card);
+        cardRepository3.save(card);
         return card;
     }
 
     public void deactivateCard(int student_id){
-        cardRepository.deactivateCard(student_id, CardStatus.DEACTIVATED.toString());
+        cardRepository3.deactivateCard(student_id, CardStatus.DEACTIVATED.toString());
     }
 }
